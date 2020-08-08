a93c5f42609a136730f91b7598975ec5dd88398d
paradisolab <brian_oppenheim@brown.edu>
2020-08-07 22:09:57 -0700

    update opencv script

--- scripts/install_opencv_411.sh
+++ scripts/install_opencv_411.sh
@@ -1,17 +1,17 @@
 folder="/usr/local/src"
 
 echo "** Remove OpenCV3.3 first"
-sudo sudo apt-get purge *libopencv*
+apt-get purge *libopencv*
 
 echo "** Install requirement"
-sudo apt-get update
-sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt-get install -y python2.7-dev python3.6-dev python-dev python-numpy python3-numpy
-sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
-sudo apt-get install -y curl qt5-default
-sudo apt-get update
+apt-get update
+apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+apt-get install -y python2.7-dev python3.6-dev python-dev python-numpy python3-numpy
+apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
+apt-get install -y curl qt5-default
+apt-get update
 
 echo "** Download opencv-4.1.1"
 cd $folder
@@ -28,7 +28,7 @@ cd release/
 cmake -D WITH_CUDA=ON -D ENABLE_PRECOMPILED_HEADERS=OFF  -D CUDA_ARCH_BIN="6.2" -D CUDA_ARCH_PTX="" -D ENABLE_FAST_MATH=ON -D CUDA_FAST_MATH=ON -D WITH_CUBLAS=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.1.1/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_GTK=OFF -D WITH_QT=ON ..
 
 make -j3
-sudo make install
+make install
 
 echo "** Installed opencv-4.1.1 successfully"
 echo "** Bye :)"
